aa6cca53b09eb412370489ce18e32d8ce9c565cc
cmjiwa <wangjinghuan@yahoo.com>
2022-07-18 23:23:55 +0300

    Modify package.json to make the vs code not auto wrap with not log line, access the inputs now submit handler can be called but cannot get input value out

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,44 @@
 class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
-  constructor() {
-    this.templateElement = document.getElementById(
-      "project-input"
-    )! as HTMLTemplateElement;
+    constructor() {
+        this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
 
-    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+        this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
+        const importedNode = document.importNode(this.templateElement.content, true);
 
-    //get the concrete element out of the template content
-    this.element = importedNode.firstElementChild as HTMLFormElement;
+        //get the concrete element out of the template content
+        this.element = importedNode.firstElementChild as HTMLFormElement;
 
-    // add an id
-    this.element.id = "user-input";
+        // add an id
+        this.element.id = "user-input";
 
-    this.attach();
-  }
-  private attach() {
-    this.hostElement.insertAdjacentElement("afterbegin", this.element);
-  }
+        //access the 3 input elements in the class
+        this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
+        this.configure();
+        this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log("form submitted = ", this.titleInputElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener("submit", this.submitHandler);
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
 }
 
 // create an instance, now in UI form shows
