05386f85f65e54f9d219767025efb1a1420efe3b
cmjiwa <wangjinghuan@yahoo.com>
2022-07-18 20:16:21 +0300

    Get template element and host element to app.ts, Import template content to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hoseElement: HTMLDivElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hoseElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+  }
+}
