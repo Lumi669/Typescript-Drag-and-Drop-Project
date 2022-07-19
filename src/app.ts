9c3b6c26be6f612e17ea04d2983f603d6a8891c9
cmjiwa <wangjinghuan@yahoo.com>
2022-07-19 12:06:00 +0300

    get userInput and check it is a tuple and destructuring get get title, description and people out for next step

--- src/app.ts
+++ src/app.ts
@@ -41,10 +41,27 @@ class ProjectInput {
         this.attach();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert("Invalid input!");
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
     @Autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log("form submitted = ", this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+        }
     }
 
     private configure() {
